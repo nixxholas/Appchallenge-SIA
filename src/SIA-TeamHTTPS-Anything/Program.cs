0d2fc3b39ea651e3174df450b50d49c04b4b7c1a
Nicholas <amperific@gmail.com>
2016-10-03 17:15:03 +0800

    Add project files.

---
+++ src/SIA-TeamHTTPS-Anything/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace SIA_TeamHTTPS_Anything
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
